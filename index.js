7f0e60a7c89079b7a63ffa4487df46dcfa66dd43
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2020-01-19 13:25:09 +0100

    feat: replaces custom prompt with external dependency

--- index.js
+++ index.js
@@ -3,18 +3,27 @@
 // Node
 const { red } = require( 'chalk' );
 
+// External
+const prompt = require( '@msg-labs/cli-prompt-list' );
+
 // Application
 const checkout = require( './src/git/checkout.js' );
-const branchSearch = require( './src/branch-search.js' );
 const getBranches = require( './src/git/branch.js' );
-const rpl = require( './src/ui/rpl.js' );
-const { write } = require( './src/ui/utils.js' );
+const renderCandidate = require( './src/ui/render-candidate.js' );
 
+
+const searchOptions = {
+    matchField: ( { name } ) => name
+};
 
 getBranches()
-    .then( branchSearch )
-    .then( rpl )
+    .then( branches => prompt(
+        branches,
+        'Branch name > ',
+        renderCandidate,
+        searchOptions
+    ) )
     .then( ( { name } ) => name )
     .then( checkout )
-    .then( response => write( `${ response }\n` ) )
-    .catch( error => write( red( error ) ) );
+    .then( response => console.log( `${ response }\n` ) )
+    .catch( error => console.log( red( error ) ) );
